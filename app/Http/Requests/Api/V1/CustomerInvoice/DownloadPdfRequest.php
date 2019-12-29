0366a3fb8102b53caefe5a9a597d66508756036e
dhovzun <shitultimate@gmail.com>
2019-12-30 03:31:13 +0600

    #14: Fixed authorize checking

--- app/Http/Requests/Api/V1/CustomerInvoice/DownloadPdfRequest.php
+++ app/Http/Requests/Api/V1/CustomerInvoice/DownloadPdfRequest.php
@@ -20,7 +20,7 @@ class DownloadPdfRequest extends FormRequest
             }])
             ->first();
 
-        return $customerWithShipments->customerShipments->count() > 0;
+        return $customerWithShipments->customerInvoices->count() > 0;
     }
 
     public function rules()
