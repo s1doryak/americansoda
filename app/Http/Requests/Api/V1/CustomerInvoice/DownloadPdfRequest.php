4851b696e8c61d17c91b0400931e75577128fc50
dhovzun <shitultimate@gmail.com>
2019-12-30 03:21:28 +0600

    #14: Implemented issue

---
+++ app/Http/Requests/Api/V1/CustomerInvoice/DownloadPdfRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\CustomerInvoice;
+
+use App\Customer;
+use App\CustomerOrder;
+use App\CustomerShipment;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class DownloadPdfRequest extends FormRequest
+{
+    public function authorize()
+    {
+        $customerWithShipments = Auth::user()
+            ->customers()
+            ->where('customer_id', $this->route('id'))
+            ->with(['customerInvoices' => function ($query) {
+                return $query->where('customer_shipment_id', $this->route('shipment_id'));
+            }])
+            ->first();
+
+        return $customerWithShipments->customerShipments->count() > 0;
+    }
+
+    public function rules()
+    {
+        return [];
+    }
+
+    public function validateResolved()
+    {
+        parent::validateResolved();
+        Customer::findOrFail($this->route('id'));
+    }
+}
