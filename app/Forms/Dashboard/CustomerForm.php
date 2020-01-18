b74390ef583a3c7ab43d8973416c6a8c1ce4433c
dhovzun <shitultimate@gmail.com>
2020-01-18 15:17:49 +0600

    Fixed groupBy

--- app/Forms/Dashboard/CustomerForm.php
+++ app/Forms/Dashboard/CustomerForm.php
@@ -227,7 +227,7 @@ class CustomerForm extends Form
                     return false === $customerPricingPolicy->trashed();
                 })
                 ->groupBy(function (CustomerPricingPolicy $customerPricingPolicy) {
-                    return $customerPricingPolicy->productGroup->getKey();
+                    return $customerPricingPolicy->productGroup->getKey() ?? null;
                 }),
         ];
 
