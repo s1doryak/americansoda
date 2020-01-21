9bff8dd9053b3abda878063f1c5b4b3ec2540f35
dhovzun <shitultimate@gmail.com>
2020-01-21 21:26:36 +0600

    Minor fix

--- app/Notifications/Api/V1/PreOrderCreate.php
+++ app/Notifications/Api/V1/PreOrderCreate.php
@@ -7,7 +7,7 @@ use App\CustomerPreOrder;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
+use Notification;
 
 /**
  * PreOrderCreate notification.
