924c0f2da962c52320d6bac4bb6b6de69a2b2d9b
Maxim Terehin <maximdevops@gmail.com>
2020-01-24 12:57:48 +0300

    Add new file

---
+++ .gitlab-deploy.prod.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Get servers list:
+set - f
+# Variables from GitLab server:
+# Note: They can't have spaces!!
+string=$DEPLOY_SERVER
+array=(${string//,/ })
+
+for i in "${!array[@]}"; do
+  echo "Deploy project on server ${array[i]}"
+  sudo -i -u demo bash << EOF
+    echo "Working On ${USER}"
+    cd /home/demo/web/gtp.americansoda.demo.crmplease.me/public_html && bash distribute.sh
+EOF
+  echo "Done"
+done
