30b034ae0574ad95b86c50221ebe629f982d1703
Maxim Terehin <maximdevops@gmail.com>
2020-02-06 15:11:16 +0300

    Update .gitlab-deploy.dev-master.sh

--- .gitlab-deploy.dev-master.sh
+++ .gitlab-deploy.dev-master.sh
@@ -2,16 +2,16 @@
 
 set - f
 
-echo "Deploy project on server ${DEPLOY_SERVER}"
+echo "Deploy project Master on Dev Server"
 sudo -i -u $PROJECT_USER bash << EOF
 	echo "Working On ${USER}"
-    cd $PROJECT_PATH &&
-    git checkout master &&
-	git pull &&
-	git pull --recurse-submodules &&
-	composer install &&
-	php artisan migrate &&
-	npm install &&
+    cd $PROJECT_PATH
+    git checkout master
+	git pull
+	git pull --recurse-submodules
+	composer install
+	php artisan migrate
+	npm install
 	npm run production
 	sudo supervisorctl restart gtp.americansoda.demo.crmplease.me-queue:gtp.americansoda.demo.crmplease.me-queue_00
 EOF
