25734b59a9ca331138bd5292c55f260bfb049e63
Maxim Terehin <maximdevops@gmail.com>
2020-02-08 14:03:14 +0300

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -4,7 +4,6 @@ ACTION_NAME=$1
 
 case "$ACTION_NAME" in
 	-m|--master)
-		shift
 		echo "Deploy project Master branch on Production server"
 		sudo -i -u $REMOTE_PROJECT_USER bash <<- EOF
 			echo "Working On ${USER}"
@@ -16,10 +15,8 @@ case "$ACTION_NAME" in
 			npm install
 			npm run production
 		EOF
-		shift
 		;;
 	-d|--dev)
-		shift
 		echo "Deploy project Dev branch on Dev server"
 		sudo -i -u $PROJECT_USER bash <<- EOF
 			echo "Working On ${USER}"
@@ -33,10 +30,8 @@ case "$ACTION_NAME" in
 			npm run production
 			sudo supervisorctl restart gtp.americansoda.demo.crmplease.me-queue:gtp.americansoda.demo.crmplease.me-queue_00
 		EOF
-		shift
 		;;
 	-dm|--devmaster)
-		shift
 		echo "Deploy project Master branch on Dev Server"
 		sudo -i -u $PROJECT_USER bash <<- EOF
 			echo "Working On ${USER}"
@@ -50,7 +45,6 @@ case "$ACTION_NAME" in
 			npm run production
 			sudo supervisorctl restart gtp.americansoda.demo.crmplease.me-queue:gtp.americansoda.demo.crmplease.me-queue_00
 		EOF
-		shift
 		;;
     *)
 		echo "Select current action"
