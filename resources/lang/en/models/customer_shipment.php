9a9171c114941b9e820ca3852051e85ca0c87b95
dhovzun <shitultimate@gmail.com>
2020-02-17 23:42:07 +0600

    Added status and number for customer_shipment translations

--- resources/lang/en/models/customer_shipment.php
+++ resources/lang/en/models/customer_shipment.php
@@ -119,6 +119,8 @@ return [
         'user' => [
             'name' => 'User',
         ],
+        'status' => 'Status',
+        'number' => 'Number',
     ],
     'statuses' => [
         'open' => 'Open',
