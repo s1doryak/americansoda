bd8b5100b0f3f73cc53e05f0c6ceb07d8954cc9b
dhovzun <shitultimate@gmail.com>
2020-02-21 21:08:54 +0600

    Fixed translations for edit stock movement form

--- app/Forms/Dashboard/StockMovementForm.php
+++ app/Forms/Dashboard/StockMovementForm.php
@@ -70,8 +70,8 @@ class StockMovementForm extends Form
 			'stockMovementProducts[0]' => [
 				'type' => 'relation_form',
 				'fields' => StockMovementProductForm::getCreateFormFields(),
-				'form_title' => trans('models/stock_movement.product.labels.plural'),
-				'resource' => 'stock_movement.product',
+				'form_title' => trans('models/stock_movement_product.labels.plural'),
+				'resource' => 'stock_movement_product',
 			],
 		];
 	}
