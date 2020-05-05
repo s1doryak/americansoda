9080819d2648c47e3027df4a892f371ff3278ab7
dhovzun <shitultimate@gmail.com>
2020-05-05 16:36:22 +0600

    Added auth attempt with wrong email notication

---
+++ app/Notifications/Api/V1/AuthFailedAttempt.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications\Api\V1;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class AuthFailedAttempt extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * @var string $email
+     */
+    protected $email;
+
+    /**
+     * AuthFailedAttempt constructor.
+     * @param string $email
+     */
+    public function __construct($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject(trans('notifications/auth_attempt_failed.subject'))
+            ->cc(config('mail.from.address'), config('mail.from.name'))
+            ->line(trans('notifications/auth_attempt_failed.message', [
+                'email' => $this->email,
+            ]));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'message' => trans('notifications/auth_attempt_failed.message')
+        ];
+    }
+}
