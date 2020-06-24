bddd230f39f603564a610b18aaab7cf1db601359
dhovzun <inspiringspidey@gmail.com>
2020-06-24 14:10:46 +0600

    Removed not existing script from template

--- resources/views/dashboard/calendar/index.blade.php
+++ resources/views/dashboard/calendar/index.blade.php
@@ -74,7 +74,4 @@
 @section('scripts')
 
     @parent
-    <!-- Calendar Script -->
-    <script src="{{ asset('vendor/material-admin/js/fullcalendar.js') }}?ver={{ config('app.version') }}"></script>
-    <script src="{{ asset('assets/dashboard/js/calendar.js') }}?ver={{ config('app.version') }}"></script>
 @stop
