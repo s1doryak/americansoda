b365eb2151ba4360c0cff28aeb09ad9ce2e70740
s1doryak <a@sidoryak.ru>
2020-06-26 13:55:42 +0300

    fix guards

--- config/auth.php
+++ config/auth.php
@@ -36,16 +36,6 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
-
-        'api' => [
-            'driver' => 'token',
-            'provider' => 'users',
-            'hash' => false,
-        ],
 
 		'app' => [
 			'driver' => 'session',
