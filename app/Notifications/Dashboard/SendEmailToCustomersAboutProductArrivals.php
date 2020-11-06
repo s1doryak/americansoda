c89825d93738c4fa67afcdaf1b6e4241637b3b0e
dhovzun <danila.hovzun.g@gmail.com>
2020-11-06 20:50:12 +0600

    Minor fix

--- app/Notifications/Dashboard/SendEmailToCustomersAboutProductArrivals.php
+++ app/Notifications/Dashboard/SendEmailToCustomersAboutProductArrivals.php
@@ -56,7 +56,7 @@ class SendEmailToCustomersAboutProductArrivals extends Notification implements S
 
         foreach ($this->products as $product) {
             $image = asset($product->product_image->getByDimension('product_image'));
-            $mail->line("- <img src='{$image}'  alt='{$product->name}'/> {$product->name}");
+            $mail->line("- <img src='{$image}' style='width: 112px; height: 112px; object-fit: contain' alt='{$product->name}'/> {$product->name}");
         }
 
         return $mail->action(
